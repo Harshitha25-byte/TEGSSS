f13915dbce6fc8be4c908c2e92a8204e8f143019
Harshitha25-byte <tharshitha57@gmail.com>
2025-03-13 15:26:04 +0530

    first commit

---
+++ TEGSSS/src/main/java/com/Harshitha25-byte/App.java
@@ -0,0 +1,13 @@
+package com.Harshitha25-byte;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
